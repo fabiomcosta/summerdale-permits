5ebd577cdb433aea1c535066343164a739f122f3
Agnel Nieves <agnelnieves@gmail.com>
2023-03-18 00:21:24 -0400

    chore: general confirguration for the project
    The configuration includes: Prettier, Eslint and Husky

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
